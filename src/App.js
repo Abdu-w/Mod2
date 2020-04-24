228a9b4e5717c1c70e1970f20b9b5ef349d69edc
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-23 22:01:00 -0400

    api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, {Component } from 'react'
+import './App.css'
+import axios from 'axios'
+
+class Countries extends Component {
+  constructor(props) {
+    super(props);  
+    this.state = {
+      data: {},
+  
+    }
+  }
+  
+  componentDidMount() {
+      axios.get(`https://api.covid19api.com/summary`).then(res => {
+        this.setState({
+          data: res.data,
+         
+        })
+        console.log(this.state.data)
+      
+      
+         
+      ;
+    }).catch(error => {
+      console.log (error)
+    })
+  
+
+
+  }
+
+  render() { 
+   
+    return (
+      <div>
+
+  
+      </div>
+  )}
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+     < Countries />
       </header>
     </div>
   );
