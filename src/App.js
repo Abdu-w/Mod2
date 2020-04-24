13237cede2cf2254387989165712e8a4729775e7
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-24 01:27:35 -0400

    brower-router

--- src/App.js
+++ src/App.js
@@ -153,6 +153,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <BrowserRouter></BrowserRouter>
         {/* < GlobalContext /> */}
         < HomePage />
         < SingleCountry/>
