249f03f0451f9c0e772e4662099deb23ef9174fd
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-24 04:51:48 -0400

    adding to footer

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function Footer(){
+    return (
+      <div className='footer'>
+         <ul>
+          <li><a href="https://ourworldindata.org/coronavirus" >world data</a></li>
+          <li><a href="https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/world-map.html" >world map</a></li>
+          <li><a href="https://www.cdc.gov/">cdc</a></li>
+        </ul>
+      </div>
+    )
+  }
+  
+export default Footer;
