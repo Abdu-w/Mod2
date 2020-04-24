d64f7dfe64bd73a2043fb2adfedc5106cd5d46e7
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-24 04:44:48 -0400

    importing component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 
-import React, {Component, createContext  } from 'react'
+import React from 'react'
 import './App.css'
-import axios from 'axios'
-import { BrowserRouter, Route, Link, Switch} from 'react-router-dom'
 
 
 
@@ -13,19 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter> 
-        {/* < GlobalContext /> */}
-        < HomePage />
-        < SingleCountry/>
-        <Navbar />
-        <Switch>
-          <Route path ='/global' component={HomePage} /> 
-          <Route path ='/country' component={SingleCountry} />
-
-        </Switch>
-        
-        
-        </BrowserRouter>
+     
       </header>
     </div>
   );
